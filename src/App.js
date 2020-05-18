6a2d59ccaf2cd4b58695d8d6119ef4b6b94a1579
German Nunez <german90nunez@gmail.com>
2020-05-17 22:29:56 -0700

    search form created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const api = {
+  key: process.env.REACT_APP_WEATHER_KEY,
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+      </main>
     </div>
   );
 }
