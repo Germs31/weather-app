4435e48a92324015a7a41ecc50920b4d7a7b969e
German Nunez <german90nunez@gmail.com>
2020-05-17 23:24:38 -0700

    weather divs styled with dummy data and date function created

--- src/App.js
+++ src/App.js
@@ -6,11 +6,38 @@ const api = {
 }
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="App">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+
+        <div className="location-box">
+          <div className="location">
+            NEW YORK USA
+          </div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+
+        <div class="weather-box">
+          <div className="temp">
+            15 deg
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
         </div>
       </main>
     </div>
